58050fbb30311ffd85b88d6e6522ba00933ff693
ramanraj00 <r02519625@gmail.com>
2026-02-13 01:14:15 +0530

    Added task-schema

---
+++ database/tasks.js
@@ -0,0 +1,40 @@
+const mongoose = require("../database/db")
+const {Schema}= mongoose;
+
+const taskSchema = new Schema({
+    
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    deadline:{
+        type:Date,
+        required:true,
+    },
+
+    completed:{
+        type:Boolean,
+        default:false
+    },
+
+   userId:{
+    type:Schema.Types.ObjectId,
+    ref:"UserCredential",
+    required:true
+   }
+    
+
+},
+
+{timestamps:true}
+
+)
+
+const taskmodel = mongoose.model("taskcredential",taskSchema);
+module.exports=taskmodel;
