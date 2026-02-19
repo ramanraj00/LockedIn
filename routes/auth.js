adb5584faad447fcc074a0687f048ae703d3f471
ramanraj00 <r02519625@gmail.com>
2026-02-19 20:44:32 +0530

    chnaged the folder structure ...Added modals folder and added...studysession.js , tasks.js and users.js from databse folder

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-const usermodel = require("../database/users")
+const usermodel = require("../models/users")
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET
