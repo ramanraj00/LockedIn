fa3ef2a5b4e7345f1432c764166627a861040c33
ramanraj00 <r02519625@gmail.com>
2026-07-09 01:43:09 +0530

    addded services

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,43 @@
 require("dotenv").config();
+console.log("1");
 
 const express = require("express");
+console.log("2");
+
 const helmet = require("helmet");
+console.log("3");
+
 const { connectDb } = require("./database/db");
+console.log("4");
 
 const authRoute = require("./routes/auth");
-const taskRoute = require("./routes/task");
-const sessionRoute = require("./routes/sessions");
-const dashboardRoute = require("./routes/dashboards")
+console.log("Auth Loaded");
 
+const taskRoute = require("./routes/task");
+console.log("Task Loaded");
+
+const sessionRoute = require("./routes/sessions");
+console.log("Session Loaded");
+
+const dashboardRoute = require("./routes/dashboards");
+console.log("Dashboard Loaded");
 const app = express();
+
+console.log("5");
 
 app.use(express.json());
 
+console.log("6 Before DB");
 connectDb();
+console.log("7 After DB Call");
+
 app.use(helmet());
+
 app.use("/api/auth", authRoute);
-app.use("/api/task",taskRoute);
-app.use("/api/session",sessionRoute);
-app.use("/api/dashboard",dashboardRoute);
+app.use("/api/task", taskRoute);
+app.use("/api/session", sessionRoute);
+app.use("/api/dashboard", dashboardRoute);
 
 app.listen(3000, () => {
-
-  console.log("Server running on port 3000");
-  
+  console.log("8 Server running on port 3000");
 });
