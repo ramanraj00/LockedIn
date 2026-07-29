e98407875757f4fa2dab056526fafbdebece86b1
ramanraj00 <r02519625@gmail.com>
2026-07-29 13:42:21 +0530

    chore: remove credential leaks

--- backend/unset_pass.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const User = require('./models/users');
-mongoose.connect('mongodb+srv://r02519625_db_user:ONU5CZiebxvTOVv2@cluster0.cggwks0.mongodb.net/').then(async () => {
-    await User.updateOne({ email: 'chodu@gmail.com' }, { $unset: { password: 1 } });
-    console.log("Password unset for chodu@gmail.com");
-    process.exit(0);
-});
