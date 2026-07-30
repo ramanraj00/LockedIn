cb9ba073f2153a998a2389326165d076be845590
ramanraj00 <r02519625@gmail.com>
2026-07-30 16:11:40 +0530

    deploying backend on render

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const cookieParser = require("cookie-parser");
 const app = express();   
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 
